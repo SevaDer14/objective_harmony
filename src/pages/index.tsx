ef3b9054013346fa5188e0380fdb50fd146bcc6c
sevader14 <vsevolod.deriushkin@gmail.com>
2021-11-12 18:03:10 +0100

    change text on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material'
 const Home = () => {
   return (
     <Typography variant="h1" sx={{ textAlign: 'center' }}>
-      Oi Boi
+      Hello World
     </Typography>
   )
 }

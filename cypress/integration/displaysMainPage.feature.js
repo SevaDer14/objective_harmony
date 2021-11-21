4bb7cc890d1957b2af741f531933583806da0a5c
sevader14 <vsevolod.deriushkin@gmail.com>
2021-11-21 13:25:24 +0100

    adds tests

--- cypress/integration/displaysMainPage.feature.js
+++ cypress/integration/displaysMainPage.feature.js
@@ -1,11 +1,44 @@
 describe('Displays Main Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
+  context('if accessed by route', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('should highlight correct navigation tab', () => {
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Home').should('have.class', 'Mui-selected')
+      })
+    })
+
+    it('should display page header and placeholder content', () => {
+      cy.get('[data-cy=page-header]').should('be.visible')
+      cy.get('[data-cy=description]').should('be.visible')
+      cy.get("[data-cy='/zen_explorer-cta']").should('be.visible')
+    })
   })
 
-  it('should display page header and placeholder content', () => {
-    cy.get('[data-cy=page-header]').should('be.visible')
-    cy.get('[data-cy=description]').should('be.visible')
-    cy.get("[data-cy='/zen_explorer-cta']").should('be.visible')
+  context('if accessed by navigation menu from zen explorer page', () => {
+    beforeEach(() => {
+      cy.visit('/zen_explorer')
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Home').click()
+      })
+    })
+
+    it('should have correct route', () => {
+      cy.url().should('contain', '/')
+    })
+
+    it('should highlight correct navigation tab', () => {
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Home').should('have.class', 'Mui-selected')
+      })
+    })
+
+    it('should display page header and placeholder content', () => {
+      cy.get('[data-cy=page-header]').should('be.visible')
+      cy.get('[data-cy=description]').should('be.visible')
+      cy.get("[data-cy='/zen_explorer-cta']").should('be.visible')
+    })
   })
 })
--- cypress/integration/zen_explorer/displaysZenExplorerApp.feature.js
+++ cypress/integration/zen_explorer/displaysZenExplorerApp.feature.js
@@ -7,6 +7,12 @@ describe('Displays "Zen Explorer" App', () => {
     it('should display page header', () => {
       cy.get('[data-cy=page-header]').should('be.visible')
     })
+
+    it('should highlight correct navigation tab', () => {
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Zen Explorer').should('have.class', 'Mui-selected')
+      })
+    })
   })
 
   context('if accessed by cta on main page', () => {
@@ -17,6 +23,35 @@ describe('Displays "Zen Explorer" App', () => {
 
     it('should have correct route', () => {
       cy.url().should('contain', '/zen_explorer')
+    })
+
+    it('should highlight correct navigation tab', () => {
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Zen Explorer').should('have.class', 'Mui-selected')
+      })
+    })
+
+    it('should display page header', () => {
+      cy.get('[data-cy=page-header]').should('be.visible')
+    })
+  })
+
+  context('if accessed by navigation menu from main page', () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Zen Explorer').click()
+      })
+    })
+
+    it('should have correct route', () => {
+      cy.url().should('contain', '/zen_explorer')
+    })
+
+    it('should highlight correct navigation tab', () => {
+      cy.get('[data-cy=navigation-tabs]').within(() => {
+        cy.contains('Zen Explorer').should('have.class', 'Mui-selected')
+      })
     })
 
     it('should display page header', () => {
